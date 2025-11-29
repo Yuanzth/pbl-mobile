ca611f16e73de809b631088c172b8516cb4101d4
mrfauzii <reishifauzi24@gmail.com>
2025-11-30 02:52:49 +0700

    feat(splash): implement splash screen UI

--- client/lib/route.dart
+++ client/lib/route.dart
@@ -1,13 +1,24 @@
 import 'package:client/screens/home_screen.dart';
 import 'package:client/screens/login_screen.dart';
+import 'package:client/screens/splash_screen.dart'; // 1. Import Splash Screen
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: "/login",
+  // 2. Ubah initialLocation ke '/splash'
+  initialLocation: "/splash",
+
   redirect: (context, state) {
-    return;
+    return null; // Biarkan null dulu jika logika ada di dalam widget Splash
   },
+
   routes: [
+    // 3. Tambahkan Route untuk Splash Screen
+    GoRoute(
+      path: "/splash",
+      name: "splash",
+      builder: (context, state) => const SplashScreen(),
+    ),
+
     GoRoute(
       path: "/",
       name: "home",
